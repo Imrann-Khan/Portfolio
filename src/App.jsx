1a91c76b1e5dd9411a00ebb0daad5b293ded3567
Imrann-Khan <ik11150500218@gmail.com>
2024-08-30 11:21:52 +0600

    created

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* navbar */}
+
+      {/* Intro */}
+
+      {/* About Me */}
+
+      {/* Skills */}
+
+      {/* Portfolio */}
+
+      {/* Companies */}
+
+      {/* Contact Us*/}
+
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
