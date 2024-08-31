bc54120d8bab822661665a3945e118bbf31471b0
MD. IMRAN-UL-HUQ <ik11150500218@gmail.com>
2024-08-31 23:26:59 +0600

    navbar incomplete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="p-2">
+      <div className="flex justify-between">
+        <img src={logo} alt="logo" className="h-7" />
+        <div>
+          <ul className="flex flex-row">
+            <li className="mr-3 transition-all ease-in-out hover:scale-110 cursor-pointer">
+              Home
+            </li>
+            <li className="mr-3 transition-all ease-in-out hover:scale-110 cursor-pointer">
+              About Me
+            </li>
+            <li className="mr-3 transition-all ease-in-out hover:scale-110 cursor-pointer">
+              Skills
+            </li>
+            <li className="mr-3 transition-all ease-in-out hover:scale-110 cursor-pointer">
+              Portfolio
+            </li>
+            <li className="mr-3 transition-all ease-in-out hover:scale-110 cursor-pointer">
+              Companies
+            </li>
+            <li className="mr-3 transition-all ease-in-out hover:scale-110 cursor-pointer">
+              Contact Me
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
